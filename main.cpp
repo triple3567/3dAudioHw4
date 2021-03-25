2f5d0cd7f4ebbc11bde686fe36823ed592d529b1
triple3567 <ericvho888@gmail.com>
2021-03-25 17:35:52 -0400

    hopefully done

--- main.cpp
+++ main.cpp
@@ -5,24 +5,55 @@
 int main(){
 
     //load input
-    string input = "./data/PROD_TAG.wav";
+    string input = "./data/new_sound.wav";
     AudioFile<float> wav = AudioFile<float>(input);
     wav.printSummary();
 
     //interpret input
     float* wav_array_left;
     float* wav_array_right;
-    interpretInput(wav, wav_array_left, wav_array_right);
+    int numSamples = wav.getNumSamplesPerChannel(); 
+    wav_array_left = new float[numSamples];
+    wav_array_right = new float[numSamples];
+
+    for (int i = 0; i < numSamples; i++)
+    {
+        wav_array_left[i] = wav.samples[0][i];
+        wav_array_right[i] = wav.samples[1][i];
+    }
 
     //apply HRTF
-    int numSamples = wav.getNumSamplesPerChannel(); 
-    float* left_processed;
-    float* right_processed;
-    left_processed = new float[numSamples+199];
-    right_processed = new float[numSamples+199];
+    vector<float> left_processed;
+    vector<float> right_processed;
     HRTFContainer hrtf = HRTFContainer();
-    left_processed = conv(wav_array_left, hrtf.get_hrir_l(0,9), numSamples, 200);
-    right_processed = conv(wav_array_right, hrtf.get_hrir_r(0,9), numSamples, 200);
+
+    int azimuth_loc[8] = {0,4,8,12,8,4,2,0};
+    int elevation = 9;
+
+    for(int i = 0; i < 8; i++){
+        if(i == 4){
+            elevation = 18;
+        }
+
+        float* curr_left = new float[numSamples/8];
+        float* curr_right = new float[numSamples/8];
+        float* curr_left_processed = new float[numSamples/8 + 199];
+        float* curr_right_processed = new float[numSamples/8 + 199];
+
+        for(int j = 0; j < numSamples / 8; j++){
+            curr_left[j] = wav_array_left[(i * numSamples/8) + j];
+            curr_right[j] = wav_array_right[(i * numSamples/8) + j];
+        }
+
+        curr_left_processed = conv(curr_left, hrtf.get_hrir_l(azimuth_loc[i],elevation), numSamples/8, 200);
+        curr_right_processed = conv(curr_right, hrtf.get_hrir_r(azimuth_loc[i],elevation), numSamples/8, 200);
+
+        for(int j = 0; j < numSamples/8 + 199; j++){
+            left_processed.push_back(curr_left_processed[j]);
+            right_processed.push_back(curr_right_processed[j]);
+        }
+
+    }
     
     //generate output wav file
     AudioFile<float> processed = AudioFile<float>();
